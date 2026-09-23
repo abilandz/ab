485900f1d7906d0a9a6af79b64366412d7746f08
abilandz <Ante.Bilandzic@cern.ch>
2026-09-23 15:48:50 +0200

    minor update

--- docs/hooks/conditional_css.py
+++ docs/hooks/conditional_css.py
@@ -6,11 +6,12 @@ def on_config(config):
 
     # Example: only load this CSS when using the "Material" theme"
     if theme_name == "material":
-        config.extra_css.append("stylesheets/material/bootstrap-like-tables.css")
+       # config.extra_css.append("stylesheets/material/bootstrap-like-tables.css")
+       config.extra_css.append("stylesheets/material/native-markdown-tables.css")
 
     # Example: only load this CSS when using the "Bootstrap" theme (default):
     elif theme_name == "bootstrap":
-        config.extra_css.append("stylesheets/bootstrap/some-settings.css")
+        config.extra_css.append("stylesheets/bootstrap/spacing.css")
 
     # Example: only load this CSS when using the "ReadTheDocs" theme:
     elif theme_name == "readthedocs":

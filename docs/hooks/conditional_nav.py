c918eb02f7750ae518cccba6b5c3bde2d145ae50
abilandz <Ante.Bilandzic@cern.ch>
2026-09-25 11:32:49 +0200

    minor update

--- docs/hooks/conditional_nav.py
+++ docs/hooks/conditional_nav.py
@@ -8,51 +8,48 @@ def on_config(config):
     if theme_name == "mkdocs": # Navigation for "bootstrap" theme (the default theme, but I have to use "mkdocs", not "bootstrap" as the name here)
         config.nav = [
             {"Introduction": "index.md"}, # this file "index.md" must be in docs/
-            {"Lecture 1": "Lecture_1.md"}, 
-            {"Lecture 2": "Lecture_2.md"},
-            {"Lecture 3": "Lecture_3.md"},
-            {"Lecture 4": "Lecture_4.md"},
+            {"Lectures": [  
+                {"Lecture 1: Trivia": "Lecture_1.md"},
+                {"Lecture 2: Commands and variables": "Lecture_2.md"},
+                {"Lecture 3: Linux file system. Positional parameters. Your first Linux/Bash command. Command precedence": "Lecture_3.md"},
+                {"Lecture 4: Loops and few other thingies": "Lecture_4.md"},
+            ]},
             {"Homeworks": [  
-                "Homeworks/Scoresheet.md",
-                "Homeworks/Homework_1.md",
-                "Homeworks/Homework_2.md",
+                {"Scoresheet": "Homeworks/Scoresheet.md"},
+                {"Homework 1: Using shell aliases as your simplest commands" : "Homeworks/Homework_1.md"},
+                {"Homework 2: User-made executables as Linux/Bash commands": "Homeworks/Homework_2.md"},
             ]},
         ]
 
     elif theme_name == "material": # Navigation for "Material" theme
         config.nav = [
             {"Introduction": "index.md"}, # this file "index.md" must be in docs/
-            {"Lecture 1": "Lecture_1.md"},
-            {"Lecture 2": "Lecture_2.md"},
-            {"Lecture 3": "Lecture_3.md"},
-            {"Lecture 444": "Lecture_4.md"},
-            {"Homeworks": [  
-                "Homeworks/Scoresheet.md",
-                "Homeworks/Homework_1.md",
-                "Homeworks/Homework_2.md",
+            {"Lectures": [  
+                {"Lecture 1: Trivia": "Lecture_1.md"},
+                {"Lecture 2: Commands and variables": "Lecture_2.md"},
+                {"Lecture 3: Linux file system. Positional parameters. Your first Linux/Bash command. Command precedence": "Lecture_3.md"},
+                {"Lecture 4: Loops and few other thingies": "Lecture_4.md"},
             ]},
-
-#            {"Homeworks": "Homeworks.md"},
-#            {"User Guide": [
-#                "guide/installation.md",
-#                "guide/usage.md",
-#                "guide/advanced.md",
-#            ]},
-#            {"API Reference": "api.md"},
-#            {"About": "about.md"},
+            {"Homeworks": [  
+                {"Scoresheet": "Homeworks/Scoresheet.md"},
+                {"Homework 1: Using shell aliases as your simplest commands" : "Homeworks/Homework_1.md"},
+                {"Homework 2: User-made executables as Linux/Bash commands": "Homeworks/Homework_2.md"},
+            ]},
         ]
 
     elif theme_name == "readthedocs": # Different structure for "ReadTheDocs" theme
         config.nav = [
             {"Introduction": "index.md"}, # this file "index.md" must be in docs/
-            {"Lecture 1": "Lecture_1.md"},
-            {"Lecture 2": "Lecture_2.md"},
-            {"Lecture 3": "Lecture_3.md"},
-            {"Lecture 4": "Lecture_4.md"},
+            {"Lectures": [  
+                {"Lecture 1: Trivia": "Lecture_1.md"},
+                {"Lecture 2: Commands and variables": "Lecture_2.md"},
+                {"Lecture 3: Linux file system. Positional parameters. Your first Linux/Bash command. Command precedence": "Lecture_3.md"},
+                {"Lecture 4: Loops and few other thingies": "Lecture_4.md"},
+            ]},
             {"Homeworks": [  
-                "Homeworks/Scoresheet.md",
-                "Homeworks/Homework_1.md",
-                "Homeworks/Homework_2.md",
+                {"Scoresheet": "Homeworks/Scoresheet.md"},
+                {"Homework 1: Using shell aliases as your simplest commands" : "Homeworks/Homework_1.md"},
+                {"Homework 2: User-made executables as Linux/Bash commands": "Homeworks/Homework_2.md"},
             ]},
         ]
 
